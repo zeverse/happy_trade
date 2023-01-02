1a56211c11de9b3207d4432035c5ae8c106c58c6
Ha Hong Son <sonhh@Has-Mac-mini.local>
2023-01-03 00:19:49 +0700

    feat(model): Add model and migration for User and Department

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatarURL',
+        'role',
+        'address',
+        'department_id'
     ];
 
     /**
@@ -40,5 +44,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'date_of_birth' => 'datetime'
     ];
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,9 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Department;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,11 +16,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->timestamp('date_of_birth');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatarURL')->nullable();
+            $table->string('role')->nullable();
+            $table->string('address')->nullable();
+            $table->foreignId('department_id')->constrained('department');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
